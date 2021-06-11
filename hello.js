1831d438508ce9d48d716b5db83f2a4e32871b59
Tiffany Williams <85643477+twilliams1025@users.noreply.github.com>
2021-06-11 01:50:27 +0000

    Added world

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
+	return "Hello world!";
 
-  let message = "Hello World!";
-  console.log(message);
+
 }
  
 module.exports = hello;
